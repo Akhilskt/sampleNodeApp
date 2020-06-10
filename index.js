f55cbf3262664f47c04ad9b93f239651d339f5af
Akhilskt <akhilcrr789@gmail.com>
2020-06-10 11:40:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const whitelist = [
     'http://localhost:80',
     'http://localhost',
     'http://localhost:8080',
-    'http://127.0.0.1:8080'
+    'http://127.0.0.1:8080',
+    'https://c720g.sse.codesandbox.io',
+    'https://362u0.csb.app'
   ];
   
   
